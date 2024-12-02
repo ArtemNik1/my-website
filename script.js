6c7722a579291908584f006587edebfee38f0726
Артем <artemaikido@gmail.com>
2024-12-02 11:58:23 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger-menu');
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+    burger.addEventListener('click', () => {
+        dropdownMenu.classList.toggle('active');
+    });
+});
